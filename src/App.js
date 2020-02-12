3dac821eaa9ed88a57bf9423213a7f4e7a1e7a50
Christian Dominguez <christian@redbonzai.com>
2020-02-11 18:12:47 -0600

    First Development:  Create new react application with basic Register, Login, and Home components.  Create routing system with react-router-dom

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <h2>Hello</h2>
+  );
+}
+
+export default App;
---
+++ src/Routes.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Register from './components/user/Register'
+import Login from './components/user/Login'
+import Home from './components/home'
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path='/' exact component={Home} />   
+                <Route path='/register' exact component={Register} />
+                <Route path='/login' exact component={Login} />
+            </Switch>
+        </BrowserRouter>)
+};
+
+export default Routes
